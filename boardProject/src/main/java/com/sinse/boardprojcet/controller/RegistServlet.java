6822128bab29d909205638366f4b51828bfefeb0
DESKTOP-9NOGIS0\a <hjin3456@naver.com>
2025-07-01 11:13:27 +0900

    init: first commit on main branch

---
+++ boardProject/src/main/java/com/sinse/boardprojcet/controller/RegistServlet.java
@@ -0,0 +1,8 @@
+package com.sinse.boardprojcet.controller;
+
+import javax.servlet.http.HttpServlet;
+
+// 클라이언트가 전송한 글쓰기 폼의 파라미터들을 받아, DB에 insert 시키는 서블릿
+public class RegistServlet extends HttpServlet{
+
+}
